cddefd59217bab5c5c352d020126ad8f128f20c5
unknown <fatinatiqahnoorazman@gmail.com>
2020-10-20 15:55:42 +0800

    graf laporan lombong

--- report/urls/state_admin.py
+++ report/urls/state_admin.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from ..views.state_admin import quarry_report_input, quarry_report, mine_report_input, mine_report
+from ..views.state_admin import quarry_report_input, quarry_report, quarry_graph, mine_report_input, mine_report, mine_graph
 
 app_name = 'state_admin'
 
 urlpatterns = [
     path('quarry/input/', quarry_report_input, name='quarry_input'),
     path('quarry/', quarry_report, name='quarry'),
+    path('quarry/graph/input/', quarry_graph, name='quarry_graph'),
     path('mine/input/', mine_report_input, name='mine_input'),
     path('mine/', mine_report, name='mine'),
+    path('mine/graph/input/', mine_graph, name='mine_graph'),
 ]
