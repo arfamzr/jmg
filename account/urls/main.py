f591fcc2e304c2c676fd0b60fbb4c821ad2f0a05
Arif <arifamzar2297@gmail.com>
2020-10-22 03:53:13 +0800

    super admin add hq admin

--- account/urls/main.py
+++ account/urls/main.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('password-change/', PasswordChangeView.as_view(), name='password-change'),
     path("state/", include('account.urls.state', namespace='state')),
     path("state-admin/", include('account.urls.state_admin', namespace='state_admin')),
+    path("super-admin/", include('account.urls.super_admin', namespace='super_admin')),
 ]
